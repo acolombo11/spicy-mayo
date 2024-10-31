d089005b694510857fc25c128b5f28f00bc727d2
Andrea Colombo <acolombodev@gmail.com>
2024-11-01 00:56:34 +0100

    Update dependencies

--- feature/events/ui/src/commonMain/kotlin/eu/acolombo/work/calendar/events/ui/EventsScreen.kt
+++ feature/events/ui/src/commonMain/kotlin/eu/acolombo/work/calendar/events/ui/EventsScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
