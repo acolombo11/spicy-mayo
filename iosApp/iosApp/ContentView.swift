b7397122d88351520ba6e9b29de799f0f6739ec5
Andrea Colombo <acolombodev@gmail.com>
2025-03-16 00:30:23 +0100

    Rename app module

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ComposeApp
+import SpicyMayoApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
