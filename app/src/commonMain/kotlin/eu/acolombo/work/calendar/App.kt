ddba6c9f0d2a0ebadcda0fce887c3a27f3345c78
Andrea Colombo <acolombodev@gmail.com>
2025-05-11 14:05:45 +0200

    Update dependencies

--- app/src/commonMain/kotlin/eu/acolombo/work/calendar/App.kt
+++ app/src/commonMain/kotlin/eu/acolombo/work/calendar/App.kt
@@ -10,11 +10,12 @@ import eu.acolombo.work.calendar.events.presentation.EventsRoute
 import eu.acolombo.work.calendar.events.presentation.EventsRouteDestination
 import eu.acolombo.work.calendar.events.presentation.EventsViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinContext
 import org.koin.compose.KoinMultiplatformApplication
 import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.dsl.KoinConfiguration
 
+@OptIn(KoinExperimentalAPI::class)
 @Suppress("PreviewPublic")
 @Composable
 @Preview
@@ -23,16 +24,14 @@ fun App() {
         KoinConfiguration { modules(koinModules) },
     ) {
         WorkCalendarTheme {
-            KoinContext {
-                NavHost(
-                    navController = rememberNavController(),
-                    startDestination = EventsRouteDestination,
-                ) {
-                    composable<EventsRouteDestination> {
-                        EventsRoute(
-                            viewModel = koinViewModel<EventsViewModel>(),
-                        )
-                    }
+            NavHost(
+                navController = rememberNavController(),
+                startDestination = EventsRouteDestination,
+            ) {
+                composable<EventsRouteDestination> {
+                    EventsRoute(
+                        viewModel = koinViewModel<EventsViewModel>(),
+                    )
                 }
             }
         }
